ec6fb614f61d24243a050d9cdfa2c786350f3f26
Pratik <pendurkarpratik04@gmail.com>
2023-07-02 21:26:19 +0530

    Introduction to JS

---
+++ Day_2/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// HIgh Level
+// Asynchronous
+// Single Threaded
+
+// Prints Hello World
+// console.log('Hello World !!!');
+
+// Variables
+// 3 ways to declare a variable
+// var
+
+// Number
+// var age = 20;
+// console.log('Age');
+// console.log(age);
+
+// String
+
+// var firstName = 'Pratik';
+// var character = 'A'
+// console.log(firstName);
+// console.log(character);
+
+// Boolean
+// true, false
+
+// var isIndian = true;
+
+// let
+
+// let name = 'Pratik';
+// // console.log(name);
+
+// let isIndian = true;
+
+// Pratik - 50
+// console.log(name + '-' + age);
+
+// const
+
+// const PI = 3.14;
+// age = 21;
+// PI = 4;
+// console.log(age);
+// console.log(PI);
+
+// operations
+
+// let num1 = 25;
+// let num2 = 20;
+
+// let ans = num1 / num2;
+
+// console.log('Answer : ' + ans);
+
+// Conditional
+
+// person : age >= 18
+
+// Conditional Programming
+// let age = 16;
+
+if (age >= 18) {
+  console.log('You are eligible');
+} else {
+  console.log('You are not eligible');
+}
+
+// 1 Second = 1000 mS
+
+console.log('I am First');
+console.log('I am Second');
+
+// Event Loop
+setTimeout(() => {
+  console.log('I am Third');
+}, 2000);
+
+console.log('I am Fourth');
